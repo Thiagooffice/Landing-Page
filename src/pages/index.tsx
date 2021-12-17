4c7dc0d8ee0e688f08dfccb64b17b4a0bcc31ff8
Thiago <thiagooffice2@gmail.com>
2021-12-17 20:48:38 -0300

    Pagina blog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/home.module.scss'
+import Image from 'next/image'
+import techsImage from '../../public/images/techs.svg'
 
 export default function Home() {
   return (
@@ -17,8 +19,38 @@ export default function Home() {
             </a>
 
           </section>
-            <img src="/images/banner-conteudos.png"
-              alt="Conteudos" />
+          <img src="/images/banner-conteudos.png"
+            alt="Conteudos" />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+          <section>
+            <h2>Aprenda criar  aplicativos para Android e iOS</h2>
+            <span>Você vai descobrir o jeito mais moderno de desenvolver apps nativos para iOS e Android, construindo aplicativos do zero até aplicativos.</span>
+          </section>
+          <img src="/images/financasApp.png" alt="Conteudos mobile" />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+
+          <img src="/images/webDev.png" alt="Desenvolvimento Web" />
+          <section>
+            <h2>Aprenda criar sistemas web</h2>
+            <span>Criar sistemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado.</span>
+          </section>
+        </div>
+
+        <div className={styles.nextLevel}>
+          <Image quality={100} src={techsImage} alt='Tecnologias' />
+          <h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
+          <span>E você vai perder a chance de evoluir de uma vez por todas?</span>
+          <a>
+            <button>COMEÇAR AGORA!</button>
+          </a>
         </div>
 
       </main>
