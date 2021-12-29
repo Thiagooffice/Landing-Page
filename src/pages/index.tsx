91339dfa0e1241cb658a8af4bb981f3ada7fa578
Thiago <thiagooffice2@gmail.com>
2021-12-29 15:11:24 -0300

    Consumindo posts e criando paginação

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,30 @@ import Head from 'next/head'
 import styles from '../styles/home.module.scss'
 import Image from 'next/image'
 import techsImage from '../../public/images/techs.svg'
+import { GetStaticProps } from 'next'
+import {getPrismicClient} from '../services/prismic'
+import Prismic from '@prismicio/client'
+import { RichText } from 'prismic-dom'
 
-export default function Home() {
+
+type Content = {
+  title: string,
+  titleContent: string,
+  linkAction: string,
+  mobileTitle: string,
+  mobileContent: string,
+  mobileBanner: string,
+  webTitle: string,
+  webContent: string,
+  webBanner: string
+}
+interface ContentProps{
+  content: Content
+}
+
+
+export default function Home({content}: ContentProps) {
+  
   return (
     <>
       <Head>
@@ -12,9 +34,9 @@ export default function Home() {
       <main className={styles.container}>
         <div className={styles.containerHeader}>
           <section className={styles.ctaText}>
-            <h1>Levando você ao próximo nível.</h1>
-            <span>Uma plataforma com cursos que vão do zero até o profissional na pratica, direto ao ponto aplicando o que usamos no mercado de trabalho. 👊</span>
-            <a>
+            <h1>{content.title}</h1>
+            <span>{content.titleContent}</span>
+            <a href={content.linkAction}>
               <button>COMEÇAR AGORA!</button>
             </a>
 
@@ -27,20 +49,20 @@ export default function Home() {
 
         <div className={styles.sectionContent}>
           <section>
-            <h2>Aprenda criar  aplicativos para Android e iOS</h2>
-            <span>Você vai descobrir o jeito mais moderno de desenvolver apps nativos para iOS e Android, construindo aplicativos do zero até aplicativos.</span>
+            <h2>{content.mobileTitle}</h2>
+            <span>{content.mobileContent}</span>
           </section>
-          <img src="/images/financasApp.png" alt="Conteudos mobile" />
+          <img src={content.mobileBanner} alt="Conteudos mobile" />
         </div>
 
         <hr className={styles.divisor} />
 
         <div className={styles.sectionContent}>
 
-          <img src="/images/webDev.png" alt="Desenvolvimento Web" />
+          <img src={content.webBanner} alt="Desenvolvimento Web" />
           <section>
-            <h2>Aprenda criar sistemas web</h2>
-            <span>Criar sistemas web, sites usando as tecnologias mais modernas e requisitadas pelo mercado.</span>
+            <h2>{content.webTitle}</h2>
+            <span>{content.webContent}</span>
           </section>
         </div>
 
@@ -48,7 +70,7 @@ export default function Home() {
           <Image quality={100} src={techsImage} alt='Tecnologias' />
           <h2>Mais de <span className={styles.alunos}>15 mil</span> já levaram sua carreira ao próximo nivel.</h2>
           <span>E você vai perder a chance de evoluir de uma vez por todas?</span>
-          <a>
+          <a href={content.linkAction}>
             <button>COMEÇAR AGORA!</button>
           </a>
         </div>
@@ -56,4 +78,44 @@ export default function Home() {
       </main>
     </>
   )
+}
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient(); 
+
+  const response = await prismic.query([
+    Prismic.Predicates.at("document.type", "home")
+  ])
+  
+  const {
+    title,
+    sub_title,
+    link_action,
+    mobile,
+    mobile_content,
+    mobile_banner,
+    title_web,
+    web_content,
+    web_banner
+  } =response.results[0].data
+
+  const content = {
+    title: RichText.asText(title),
+    titleContent: RichText.asText(sub_title),
+    linkAction: link_action.url,
+    mobileTitle: RichText.asText(mobile),
+    mobileContent: RichText.asText(mobile_content),
+    mobileBanner: mobile_banner.url,
+    webTitle: RichText.asText(title_web),
+    webContent: RichText.asText(web_content),
+    webBanner: web_banner.url
+
+
+  }
+
+  return{
+    props:{
+      content
+    },
+    revalidate: 60 * 2 // A cada 2 minutos 
+  }
 }
